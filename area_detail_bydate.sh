98850dc20b2bf0dc37ce1e0d50d14e5b213c88c1
mamengxiao <470491409@qq.com>
2023-10-18 16:37:16 +0800

    1.受访页面summary新增title 2.地域分析新增city

--- area_detail_bydate.sh
+++ area_detail_bydate.sh
@@ -76,16 +76,18 @@ SELECT '${cal_date}' AS stat_date
 	, If(t2.is_first_day = '', 'all', t2.is_first_day) AS is_first_day
 	, multiIf(t2.country = '', 'all', t2.country = 'N/A', '未知国家', t2.country) AS country
 	, multiIf(t2.province = '', 'all', t2.province = 'N/A', '未知省份', t2.province) AS province
+	, multiIf(t2.city = '', 'all', t2.city = 'N/A', '未知城市', t2.city) AS city
 	, t2.pv, t4.visitCount, t2.uv, t2.new_uv, t2.ipCount
 	, t4.visitTime, t4.bounce,NOW() AS update_time
 FROM (
-	SELECT lib, project_name, is_first_day, country, province
+	SELECT lib, project_name, is_first_day, country, province,city
 		, count(t1.pv) AS pv, COUNTDistinct(t1.user) AS uv
 		, COUNTDistinct(t1.new_user) AS new_uv, COUNTDistinct(t1.client_ip) AS ipCount
 	FROM (
 		SELECT lib, project_name, is_first_day,
 		  if(country = '', 'N/A', country) AS country
 			, if(province = '', 'N/A', province) AS province
+			, if(city = '', 'N/A', city) AS city
 			, multiIf(lib = 'js'
 				AND event = '\$pageview', event, lib IN ('iOS', 'Android')
 				AND event = '\$AppViewScreen', event, lib = 'MiniProgram'
@@ -105,16 +107,17 @@ FROM (
 		FROM ${ck_log_db}log_analysis
 		WHERE stat_date = '${cal_date}'
 	) t1
-	GROUP BY lib, project_name, is_first_day, country, province WITH CUBE
+	GROUP BY lib, project_name, is_first_day, country, province,city WITH CUBE
 ) t2
 	LEFT JOIN (
-		SELECT lib, project_name, is_first_day, country, province
+		SELECT lib, project_name, is_first_day, country, province,city
 			, count(1) AS visitCount, sum(diff) AS visitTime
 			, sum(multiIf(pv = 1, 1, 0)) AS bounce
 		FROM (
 			SELECT lib, project_name, is_first_day
 				, if(country = '', 'N/A', country) AS country
 				, if(province = '', 'N/A', province) AS province 
+				, if(city = '', 'N/A', city) AS city 
 				, arraySort(groupUniqArray(stat_date)) AS stat_dates
 				, max(log_time) - min(log_time) AS diff
 				, count(1) AS pv
@@ -122,15 +125,16 @@ FROM (
 			WHERE stat_date <= '${cal_date}'
 				AND stat_date >= '${previous_date}'
 				AND event_session_id <> ''
-			GROUP BY event_session_id, lib, project_name, is_first_day, country, province
+			GROUP BY event_session_id, lib, project_name, is_first_day, country, province,city 
 		) t3
 		WHERE indexOf(stat_dates, toDate('${cal_date}')) = 1
-		GROUP BY lib, project_name, is_first_day, country, province WITH CUBE
+		GROUP BY lib, project_name, is_first_day, country, province,city WITH CUBE
 	) t4
 	ON t2.lib = t4.lib
 		AND t2.project_name = t4.project_name
 		AND t2.country = t4.country
 		AND t2.province = t4.province
+		AND t2.city = t4.city 
 		AND t2.is_first_day = t4.is_first_day"
    echo ${cur_date_sql}
 
--- visituri_summary_bydate.sh
+++ visituri_summary_bydate.sh
@@ -73,19 +73,22 @@ do
 SELECT '${cal_date}' AS stat_date
 	, If(t2.lib = '', 'all', t2.lib) AS lib
 	, multiIf(t2.project_name = '', 'all', t2.project_name = 'N/A', '', t2.project_name) AS project_name
-	,multiIf(t2.url = '', 'all', t2.url = 'N/A', '', t2.url) AS url, t2.pv,  NOW() AS update_time
+	,multiIf(t2.url = '', 'all', t2.url = 'N/A', '', t2.url) AS url,
+	multiIf(t2.title = '', 'all', t2.title = 'N/A', '', t2.title) AS title,
+	 t2.pv,  NOW() AS update_time
 FROM (
-	SELECT lib, project_name, url,count(t1.pv) AS pv 
+	SELECT lib, project_name, url,count(t1.pv) AS pv,title 
 	FROM (
 		SELECT lib, project_name,if(url = '', 'N/A', url) AS url
 			, multiIf(lib = 'js'
 				AND event = '\$pageview', event, lib IN ('iOS', 'Android')
 				AND event = '\$AppViewScreen', event, lib = 'MiniProgram'
-				AND event = '\$MPViewScreen', event, NULL) AS pv 
+				AND event = '\$MPViewScreen', event, NULL) AS pv
+				,if(title = '', 'N/A', title) AS title 
 		FROM ${ck_log_db}log_analysis
 		WHERE stat_date = '${cal_date}'
 	) t1
-	GROUP BY lib, project_name,url WITH CUBE
+	GROUP BY lib, project_name,url,title WITH CUBE
 ) t2"
   # echo ${cur_date_sql}
 
